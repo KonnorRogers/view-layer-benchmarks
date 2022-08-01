3960b12efb4c738a39ce32442ddd24c1ca53fba4
ParamagicDev <konnor5456@gmail.com>
2022-08-01 19:01:36 -0400

    cached template

--- templates/template.rb
+++ templates/template.rb
@@ -1,6 +1,6 @@
 class Template
   def render
     @_template ||= template
-    @_render ||= eval(Erubi::Engine.new(template).src).html_safe
+    @_render ||= eval(Erubi::Engine.new(@_template).src).html_safe
   end
 end
